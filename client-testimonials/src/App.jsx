b8933974ce49230d4aca983b7807a9a2cd58e105
Ankitkr <ankitkr1001a@gmail.com>
2025-02-24 15:25:54 +0530

    done

---
+++ client-testimonials/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from "./component/TestimonialCard"
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1>Our Testimonial:</h1>
+    <Card name="Sumit" review="This service was fantastic!!"/>
+    <Card name="Zack" review="Service was good,I will recommend this service"/>
+    </>
+  )
+}
+
+export default App
---
+++ client-testimonials/src/component/TestimonialCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TestimonialCard({name,review}) {
+  return (
+    <>
+    <h2>CustomerName: {name}</h2>
+    <h3>Review: {review}</h3>
+    </>
+    
+  )
+}
+
+export default TestimonialCard
